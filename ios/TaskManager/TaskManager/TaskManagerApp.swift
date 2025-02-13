acab085bfc11af7f24d4584f15a87f66ecf97a14
Abhishek Dagwar <47711239+abhiDagwar@users.noreply.github.com>
2025-02-13 03:21:04 -0800

    Created basic file structure with standard SWIFTUI template after deleting submodule

---
+++ ios/TaskManager/TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Abhishek G on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
